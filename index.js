a222778721a77e089ee60df1646a4631d7f968ba
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-03-18 21:08:10 +0100

    Mise à jour des fichiers

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded',(event)=>{
 let btn = document.querySelector("input[type='button']")
 let result = document.querySelector('.weather')
 let img = document.querySelector('.img')
-let funImg = document.querySelector('.animate')
+let funImg = document.querySelector('.fig')
+let displayRes = document.querySelector('.return')
 
 // Utlisation de l'objet.
 const req = new XMLHttpRequest()
@@ -20,74 +21,82 @@ Pour la température en Celsius, utilisez des unités = metric
 let unit = "metric"
 let ville = document.querySelector("#city")
 
+/*******************************
+ Fonction de requête vers l'API.
+*******************************/
 
-// Fonction de requête vers l'API.
+// Vériier le champ 
+const checkBtn = ()=> ville.value != ""  ? showWeather(ville.value,unit) : alert('champ vide')
+
+// Appel vers l'API si champ rempli
 const showWeather = (city,temp)=>{
-    funImg.src=""
 
-    req.open('GET',`http://api.openweathermap.org/data/2.5/weather?q=${city},fr&units=${temp}&lang=fr&appid=yourtoken`,true)
+
+    req.open('GET',`http://api.openweathermap.org/data/2.5/weather?q=${city},fr&units=${temp}&lang=fr&appid=0acd37018790aafd9883a9cb3709adb3`,true)
 
 // Tester le retour http    
     req.onreadystatechange = () =>{
 
         if ( req.readyState ===XMLHttpRequest.DONE && req.status ===404){
 
-            let response  = JSON.parse(req.responseText)
+            let obj  = JSON.parse(req.responseText)
             let errMsg = `${city} ne correspond à aucune ville de France`
             let error = "Une erreur est survenue"
 
-            const err = (response.message === 'city not found') ? result.innerText=errMsg  : result.innerText = error 
+            const err = (obj.message === 'city not found') ? result.innerText=errMsg  : result.innerText = error 
         }
 
         else if(req.readyState === XMLHttpRequest.DONE && req.status === 200) {
 
-            let response  = JSON.parse(req.responseText)
+            let obj  = JSON.parse(req.responseText)
 
-            let minTemp = response.main.temp_min
+            let minTemp = obj.main.temp_min
 
 // Température actuel
-            let maxTemp =response.main.temp_max
+            let maxTemp =obj.main.temp_max
 
 // Description du temps actuel
-            let meteo = response.weather[0].description
+            let meteo = obj.weather[0].description
 
 // Id du temps
-            let idMeteo = response.weather[0].id
+            let idMeteo = obj.weather[0].id
 
 // Id de l'icon météo            
-            let icon = response.weather[0].icon
+            let icon = obj.weather[0].icon
 
 // Insertion de l'Id             
             let imgUrl = `http://openweathermap.org/img/w/${icon}.png`
             
-            console.log(idMeteo)
-            result.innerText =`${meteo} avec une température maximum de ${maxTemp}°` 
+            result.innerText =`${meteo} avec une température maximum de ${maxTemp}°`.toUpperCase()
             img.src=imgUrl
             showImg(idMeteo)
+            displayRes.style.display="block"
+
         } 
     }
     req.send(null)
 }
 
-
-// Afficher image animée selon l'id météo.
-const showImg = (id)=>{
-
-    switch (id){
-
-        case 800 :
-            funImg.src= "img/homer_sun.gif"
-        break ;
-
-        case 601 ||615 || 616 :
-            funImg.src = "img/homer_cold.gif"
-        break;     
-    }
-}
+/*******************************
+CONDITIONS SELON L'Id METEO
+*******************************/
+const showImg = (id)=> id >=500 && id <= 532 ? funImg.src="img/rain.gif" : imgCold(id)
+const imgCold = (id)=> id >=600 && id <= 622 ? funImg.src="img/snow.gif" : imgSun(id)
+const imgSun  = (id)=> id === 800 || id === 801 ? funImg.src="img/sun.gif" : imgCloud(id)
+const imgCloud = (id)=> id >801 && id < 805 ? funImg.src="img/cloud.gif" : ""
 
 
 // Ecoute du click.
-btn.addEventListener('click',()=> showWeather(ville.value,unit))
-
+btn.addEventListener('click',()=> checkBtn())
 
 })
+
+    
+
+
+
+
+
+
+
+
